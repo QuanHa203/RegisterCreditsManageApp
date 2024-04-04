8f93bba8cdb4463a083f5e1e924b290f9dfd8830
MinhQuan124 <dminhquan2003@gmail.com>
2024-04-04 12:38:47 +0700

    .

---
+++ RegisterCreditsManageApp/Windows/LoginWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace RegisterCreditsManageApp.Windows
+{
+    /// <summary>
+    /// Interaction logic for LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AccountTextbox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox textValue = (TextBox)sender;
+            if (textValue.Text == "Your account")
+            {
+                textValue.Text = "";
+                textValue.Foreground = Brushes.White;
+            }
+        }
+        private void AccountTextbox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox textValue = (TextBox)sender;
+            if (string.IsNullOrWhiteSpace(textValue.Text))
+            {
+                textValue.Text = "Your account";
+                textValue.Foreground = Brushes.Gray;
+            }
+        }
+
+        private void PasswordTextbox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox textValue = (TextBox)sender;
+            if (textValue.Text == "Password")
+            {
+                textValue.Text = "";
+                textValue.Foreground = Brushes.White;
+            }
+        }
+
+        private void PasswordTextbox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox textValue = (TextBox)sender;
+            if (string.IsNullOrWhiteSpace(textValue.Text))
+            {
+                textValue.Text = "Password";
+                textValue.Foreground = Brushes.Gray;
+            }
+        }
+
+    }
+}
