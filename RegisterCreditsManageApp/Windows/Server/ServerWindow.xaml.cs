8a0f5279eeec1ed97cefd3d68e440f51c3b0579c
quan ha <haquanbg203@gmail.com>
2024-04-05 21:19:23 +0700

    Update ServerWindow, ClientWindow

--- RegisterCreditsManageApp/Windows/Server/ServerWindow.xaml.cs
+++ RegisterCreditsManageApp/Windows/Server/ServerWindow.xaml.cs
@@ -1,20 +1,9 @@
 ﻿using RegisterCreditsManageApp.Resources;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Security.Cryptography;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace RegisterCreditsManageApp.Windows.Server
 {
@@ -31,6 +20,7 @@ namespace RegisterCreditsManageApp.Windows.Server
 
         private void ThemeModeCheckBox_Unchecked(object sender, RoutedEventArgs e)
         {
+            LinearGradientBrush b = new LinearGradientBrush();
             DoubleAnimationOfThemeModeCheckBox();
             SetTheme.SetThemeMode(SetTheme.ThemeMode.DarkMode);
         }
@@ -46,9 +36,9 @@ namespace RegisterCreditsManageApp.Windows.Server
         /// </summary>
         private void DoubleAnimationOfThemeModeCheckBox()
         {
-            Grid iconThemeMode = ThemeModeCheckBox.Template.FindName("IconThemeMode", ThemeModeCheckBox) as Grid;            
+            Grid iconThemeMode = ThemeModeCheckBox.Template.FindName("IconThemeMode", ThemeModeCheckBox) as Grid;
             // Get Parent of iconThemeMode
-            FrameworkElement parentElement = VisualTreeHelper.GetParent(iconThemeMode) as FrameworkElement;            
+            FrameworkElement parentElement = VisualTreeHelper.GetParent(iconThemeMode) as FrameworkElement;
             if (iconThemeMode != null && parentElement != null)
             {
                 double from;
@@ -76,6 +66,71 @@ namespace RegisterCreditsManageApp.Windows.Server
                 // Áp dụng animation vào X của TranslateTransform
                 translateTransform.BeginAnimation(TranslateTransform.XProperty, animation);
             }
+        }
+
+        private void LeftMenuBtnMouseLeaveAnimation(Button btn)
+        {
+            Border border = btn.Content as Border;
+            LinearGradientBrush brush = border.Background as LinearGradientBrush;
+            //border.BorderBrush = new SolidColorBrush(Color.FromArgb(0, 0, 0, 0));
+
+            DoubleAnimation animation = new DoubleAnimation();
+            animation.From = 1;
+            animation.To = 0;
+            animation.Duration = TimeSpan.FromMilliseconds(100);
+            
+            brush.GradientStops[1].BeginAnimation(GradientStop.OffsetProperty, animation);
+            border.BorderBrush.BeginAnimation(SolidColorBrush.OpacityProperty, animation);
+        }
+
+        private void LeftMenuBtnMouseEnterAnimation(Button btn)
+        {
+            Border border = btn.Content as Border;
+            LinearGradientBrush brush = border.Background as LinearGradientBrush;                                   
+            //border.BorderBrush = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#3663fe"));
+            
+            DoubleAnimation animation = new DoubleAnimation();
+            animation.From = 0;
+            animation.To = 1;
+            animation.Duration = TimeSpan.FromMilliseconds(100);
+            
+            brush.GradientStops[1].BeginAnimation(GradientStop.OffsetProperty, animation);
+            border.BorderBrush.BeginAnimation(SolidColorBrush.OpacityProperty, animation);
+        }
+
+        private void LeftMenuBtn_MouseLeave(object sender, MouseEventArgs e)
+        {
+            LeftMenuBtnMouseLeaveAnimation((Button)sender);
+        }
+
+        private void LeftMenuBtn_MouseEnter(object sender, MouseEventArgs e)
+        {
+            LeftMenuBtnMouseEnterAnimation((Button)sender);
+        }
+
+        private void HomeBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AnalysisBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ManagementBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ExitBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AccountBtn_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
