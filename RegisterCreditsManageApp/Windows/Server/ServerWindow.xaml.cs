9e3da9d58c0943a1d8e1358c96ef44a32f0d06b1
quan ha <haquanbg203@gmail.com>
2024-04-07 00:12:49 +0700

    Clean code ServerWindow..xamlcs, ClientWindow.xaml.cs and Fix some bug

--- RegisterCreditsManageApp/Windows/Server/ServerWindow.xaml.cs
+++ RegisterCreditsManageApp/Windows/Server/ServerWindow.xaml.cs
@@ -1,116 +1,38 @@
 ﻿using RegisterCreditsManageApp.Resources;
+using System;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace RegisterCreditsManageApp.Windows.Server
 {
     /// <summary>
     /// Interaction logic for ServerWindow.xaml
     /// </summary>
-    public partial class ServerWindow : Window
+    public partial class ServerWindow : MainWindow
     {
-        public ServerWindow()
+        public ServerWindow() : base()
         {
             InitializeComponent();
 
         }
 
-        private void ThemeModeCheckBox_Unchecked(object sender, RoutedEventArgs e)
+        protected override void ThemeModeCheckBox_Checked(object sender, RoutedEventArgs e)
         {
-            LinearGradientBrush b = new LinearGradientBrush();
-            DoubleAnimationOfThemeModeCheckBox();
-            SetTheme.SetThemeMode(SetTheme.ThemeMode.DarkMode);
+            base.ThemeModeCheckBox_Checked(sender, e);
         }
 
-        private void ThemeModeCheckBox_Checked(object sender, RoutedEventArgs e)
+        protected override void ThemeModeCheckBox_Unchecked(object sender, RoutedEventArgs e)
         {
-            DoubleAnimationOfThemeModeCheckBox();
-            SetTheme.SetThemeMode(SetTheme.ThemeMode.LightMode);
-        }
-
-        /// <summary>
-        /// Animation of ThemeModeCheckBox(Dark mode, Light mode button)
-        /// </summary>
-        private void DoubleAnimationOfThemeModeCheckBox()
-        {
-            Grid iconThemeMode = ThemeModeCheckBox.Template.FindName("IconThemeMode", ThemeModeCheckBox) as Grid;
-            // Get Parent of iconThemeMode
-            FrameworkElement parentElement = VisualTreeHelper.GetParent(iconThemeMode) as FrameworkElement;
-            if (iconThemeMode != null && parentElement != null)
-            {
-                double from;
-                double to;
-                if (iconThemeMode.RenderTransform.Value.OffsetX >= 0)
-                {
-                    from = 0;
-                    to = -30;
-                }
-                else
-                {
-                    from = -30;
-                    to = 0;
-                }
-
-                TranslateTransform translateTransform = new TranslateTransform();
-                iconThemeMode.RenderTransform = translateTransform;
-
-                // Tạo DoubleAnimation cho thay đổi X của TranslateTransform
-                DoubleAnimation animation = new DoubleAnimation();
-                animation.From = from; // Giá trị ban đầu của X
-                animation.To = to; // Giá trị cuối cùng của X
-                animation.Duration = TimeSpan.FromMilliseconds(100); // Thời gian thực hiện animation
-
-                // Áp dụng animation vào X của TranslateTransform
-                translateTransform.BeginAnimation(TranslateTransform.XProperty, animation);
-            }
-        }
-
-        private void LeftMenuBtnMouseLeaveAnimation(Button btn)
-        {
-            Border border = btn.Content as Border;
-            LinearGradientBrush brush = border.Background as LinearGradientBrush;
-            //border.BorderBrush = new SolidColorBrush(Color.FromArgb(0, 0, 0, 0));
-
-            DoubleAnimation animation = new DoubleAnimation();
-            animation.From = 1;
-            animation.To = 0;
-            animation.Duration = TimeSpan.FromMilliseconds(100);
-            
-            brush.GradientStops[1].BeginAnimation(GradientStop.OffsetProperty, animation);
-            border.BorderBrush.BeginAnimation(SolidColorBrush.OpacityProperty, animation);
-        }
-
-        private void LeftMenuBtnMouseEnterAnimation(Button btn)
-        {
-            Border border = btn.Content as Border;
-            LinearGradientBrush brush = border.Background as LinearGradientBrush;                                   
-            //border.BorderBrush = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#3663fe"));
-            
-            DoubleAnimation animation = new DoubleAnimation();
-            animation.From = 0;
-            animation.To = 1;
-            animation.Duration = TimeSpan.FromMilliseconds(100);
-            
-            brush.GradientStops[1].BeginAnimation(GradientStop.OffsetProperty, animation);
-            border.BorderBrush.BeginAnimation(SolidColorBrush.OpacityProperty, animation);
-        }
-
-        private void LeftMenuBtn_MouseLeave(object sender, MouseEventArgs e)
-        {
-            LeftMenuBtnMouseLeaveAnimation((Button)sender);
-        }
-
-        private void LeftMenuBtn_MouseEnter(object sender, MouseEventArgs e)
-        {
-            LeftMenuBtnMouseEnterAnimation((Button)sender);
+            base.ThemeModeCheckBox_Unchecked(sender, e);
         }
 
         private void HomeBtn_Click(object sender, RoutedEventArgs e)
         {
-
+            
         }
 
         private void AnalysisBtn_Click(object sender, RoutedEventArgs e)
